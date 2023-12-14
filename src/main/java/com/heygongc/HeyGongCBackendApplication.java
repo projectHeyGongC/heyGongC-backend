b269e4e4f1bf69afdd66ba5ec7298aeec91b1a8c
sbywww <binyvvv@gmail.com>
2023-12-15 00:26:21 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/com/heygongc/HeyGongCBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heygongc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyGongCBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeyGongCBackendApplication.class, args);
+    }
+
+}
