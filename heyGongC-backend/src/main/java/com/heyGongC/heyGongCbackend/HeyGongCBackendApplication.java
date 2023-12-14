4420345df46038dbba6e9b07dc08796754e1ac1b
Zeeeen0 <hoo1042@gmail.com>
2023-12-14 22:38:37 +0900

    first commit

---
+++ heyGongC-backend/src/main/java/com/heyGongC/heyGongCbackend/HeyGongCBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heyGongC.heyGongCbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyGongCBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyGongCBackendApplication.class, args);
+	}
+
+}
---
+++ heyGongC-backend/src/test/java/com/heyGongC/heyGongCbackend/HeyGongCBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyGongC.heyGongCbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyGongCBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
