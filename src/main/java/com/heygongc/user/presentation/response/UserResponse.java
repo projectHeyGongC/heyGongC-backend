a2be5db5fc2fd569709b01d1e0ca3a08be36a4d3
Zeeeen0 <hoo1042@gmail.com>
2024-01-17 21:06:28 +0900

    Swagger description 추가

--- src/main/java/com/heygongc/user/presentation/response/UserResponse.java
+++ src/main/java/com/heygongc/user/presentation/response/UserResponse.java
@@ -1,11 +1,20 @@
 package com.heygongc.user.presentation.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "사용자 정보 응답")
 public record UserResponse(
+        @Schema(description = "디바이스 ID")
         String deviceId,
+        @Schema(description = "디바이스 OS")
         String deviceOs,
+        @Schema(description = "가입한 SNS 종류", allowableValues = {"GOOGLE", "APPLE"})
         String snsType,
+        @Schema(description = "이메일")
         String email,
+        @Schema(description = "알람 수신 여부", allowableValues = {"true", "false"})
         Boolean alarm,
+        @Schema(description = "마케팅 정보 수신 여부", allowableValues = {"true", "false"})
         Boolean ads
 ) {
 }
