8dd2b752593ce280f318ab0b8c90d26f0257f396
Zeeeen0 <hoo1042@gmail.com>
2024-01-18 02:35:28 +0900

    github action 설정 테스트

--- src/test/java/com/heygongc/HeyGongCBackendApplicationTests.java
+++ src/test/java/com/heygongc/HeyGongCBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.heygongc;
 
+import com.heygongc.user.presentation.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class HeyGongCBackendApplicationTests {
 
+    @Autowired
+    private UserController userController;
+
     @Test
     void contextLoads() {
+        assertNotNull(userController);
     }
 
 }
