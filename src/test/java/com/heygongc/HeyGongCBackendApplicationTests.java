4d39f0cd0afed9fcfa0243cad1493d8c1c3b24b8
Zeeeen0 <hoo1042@gmail.com>
2024-01-18 03:15:21 +0900

    github action 설정 테스트

--- src/test/java/com/heygongc/HeyGongCBackendApplicationTests.java
+++ src/test/java/com/heygongc/HeyGongCBackendApplicationTests.java
@@ -4,10 +4,12 @@ import com.heygongc.user.presentation.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/config/application.yml") // 테스트에 사용할 프로퍼티 파일 경로 지정
 class HeyGongCBackendApplicationTests {
 
     @Autowired
