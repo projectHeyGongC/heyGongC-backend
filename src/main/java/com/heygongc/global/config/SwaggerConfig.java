feec79a826e0ec10d39b2d0cd95b892cd9318186
sbywww <binyvvv@gmail.com>
2024-02-17 15:30:07 +0900

    Swagger Bearer 처리

--- src/main/java/com/heygongc/global/config/SwaggerConfig.java
+++ src/main/java/com/heygongc/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.heygongc.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +13,20 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI api() {
-        SecurityScheme apiKey = new SecurityScheme()
-                .type(SecurityScheme.Type.APIKEY)
+        Info info = new Info();
+        info.title("HeyGongC Api Documentation").description("어이 공씨 API 문서입니다.").version("1.0.0");
+
+        SecurityScheme securityScheme = new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER)
                 .name("Authorization");
 
-        SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("Bearer Token");
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("jwt");
 
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-                .addSecurityItem(securityRequirement);
+                .components(new Components().addSecuritySchemes("jwt", securityScheme))
+                .addSecurityItem(schemaRequirement)
+                .info(info);
     }
 }
