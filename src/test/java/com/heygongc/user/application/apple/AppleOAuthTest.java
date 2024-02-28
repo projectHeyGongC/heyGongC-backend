662da9a9e85d6109fd2629cb2eead9279f76b408
Zeeeen0 <hoo1042@gmail.com>
2024-02-29 01:09:55 +0900

    test 오류 수정 테스트

--- src/test/java/com/heygongc/user/application/apple/AppleOAuthTest.java
+++ src/test/java/com/heygongc/user/application/apple/AppleOAuthTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Objects;
@@ -11,6 +12,7 @@ import java.util.Objects;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppleOAuthTest {
 
     @Autowired
