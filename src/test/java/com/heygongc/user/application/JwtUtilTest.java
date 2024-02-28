2de13d8a4ceffa0451625fc35064373fbe5bc7af
Zeeeen0 <hoo1042@gmail.com>
2024-02-29 01:10:31 +0900

    test 오류 수정 테스트

--- src/test/java/com/heygongc/user/application/JwtUtilTest.java
+++ src/test/java/com/heygongc/user/application/JwtUtilTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class JwtUtilTest {
 
