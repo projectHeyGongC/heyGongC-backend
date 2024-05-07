43885a19340c9e91a17429391fec0d3017ff3c1b
Zeeeen0 <hoo1042@gmail.com>
2024-05-07 23:00:35 +0900

    [feature/HEYGC-104] 테스트용 로그인 추가

--- src/main/java/com/heygongc/common/TestController.java
+++ src/main/java/com/heygongc/common/TestController.java
@@ -1,23 +1,31 @@
 package com.heygongc.common;
 
+import com.heygongc.auth.domain.AuthToken;
 import com.heygongc.global.infra.FirebaseCloudMessaging;
 import com.heygongc.global.infra.FirebaseData;
+import com.heygongc.user.application.UserService;
+import com.heygongc.user.presentation.response.TokenResponse;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Hidden
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     private final FirebaseCloudMessaging firebaseCloudMessaging;
+    private final UserService userService;
 
-    public TestController(FirebaseCloudMessaging firebaseCloudMessaging) {
+    public TestController(FirebaseCloudMessaging firebaseCloudMessaging, UserService userService) {
         this.firebaseCloudMessaging = firebaseCloudMessaging;
+        this.userService = userService;
     }
 
-    @PostMapping("/test/push")
+    @PostMapping("/push")
     public void pushTest(@RequestBody TestPushDto dto) throws Exception {
         firebaseCloudMessaging.sendMessage(
                 FirebaseData.builder()
@@ -29,11 +37,23 @@ public class TestController {
                         .build());
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> testLogin(@RequestBody TestLoginDto dto) {
+        AuthToken authToken = userService.testLogin(dto.userSeq, dto.snsId, dto.email);
+        return ResponseEntity.ok()
+                .body(
+                        new TokenResponse(authToken.getAccessToken(), authToken.getRefreshToken())
+                );
+    }
+
 
     public record TestPushDto(String token,
-                                 String title,
-                                 String body,
-                                 String action,
-                                 String content) {
-    }
+                              String title,
+                              String body,
+                              String action,
+                              String content) {}
+
+    public record TestLoginDto(Long userSeq,
+                               String email,
+                               String snsId) {}
 }
--- src/main/java/com/heygongc/user/domain/repository/UserRepository.java
+++ src/main/java/com/heygongc/user/domain/repository/UserRepository.java
@@ -10,6 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserSeq(Long seq);
 
-
     boolean existsBySnsId(String snsId);
 }
